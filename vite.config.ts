c5693517ed95a85126bd810782c31780521af85f
hintonevolutiontechdgp-cell <hintonevolutiontechdgp@gmail.com>
2026-09-06 01:10:05 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { ViteWebfontDownload } from 'vite-plugin-webfont-dl';
 import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    plugins: [react(), tailwindcss()],
+    plugins: [
+      react(), 
+      tailwindcss(),
+      ViteWebfontDownload()
+    ],
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
@@ -17,6 +22,7 @@ export default defineConfig(() => {
     server: {
       
       hmr: process.env.DISABLE_HMR !== 'true',
+      
       watch: process.env.DISABLE_HMR === 'true' ? null : {},
     },
   };
